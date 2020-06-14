7d6a16e0e8682449d9914fbc8cd5c30ecab62e91
Matthew M <mbmoorin@gmail.com>
2020-06-14 19:10:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ const playlist = {
   artist:'title'
 }
 
-function updatePlaylist(playlist, artist, title) {
+function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist,{artist: title})
   }
 
